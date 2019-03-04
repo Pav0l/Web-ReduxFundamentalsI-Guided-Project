674cc7f2e1619cf4649dfe9c5436cb947d1e9442
Pav0l <pavol.karas@gmail.com>
2019-03-04 12:06:46 +0100

    lecture notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore, combineReducers } from 'redux';
-// import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import Container from './components/Container';
 
+// 1. fucntion that returns the initial state:
+// it takes reducers as arguments
+const combinedState = combineReducers({ mentalHealth, physicalHealth });
 
-// const store = createStore(
-//   combinedState,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-// );
+// the result should be the same as this simple fn below:
+// function combinedState() {
+//   return {
+//     mentalHealth: 100,
+//     physicalHealth: 100,
+//   };
+// }
 
+// function that will take a state and a yell(action)
+// this function HAVE TO BE PURE!
+// these are REDUCERS
+function mentalHealth(state = 100, action) {
+  switch (action.type) {
+    case 'WATCH_THE_NEWS':
+      return state - 10;
+    case 'WIN_THE_LOTTERY':
+      return state + 20;
+    default:
+      return state;
+  }
+}
+
+function physicalHealth(state = 100, action) {
+  switch (action.type) {
+    case 'SLIP_ON_BANANA_PEEL':
+      return state - 10;
+    case 'WIN_THE_LOTTERY':
+      return state + 10;
+    default:
+      return state;
+  }
+}
+
+
+// 2. creates state object in REDUX
+const store = createStore(
+  combinedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+
+// 3. we need to wrap our app in a Provider
+// Provider takes a createStore prop
 ReactDOM.render(
-  <Container />,
+  <Provider store={store}>
+    <Container />
+  </Provider>,
   document.querySelector('#target'),
 );
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,24 +1,89 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Action Createors - usually stored in separate file
+const sliponBananaPeel = () => ({
+  type: 'SLIP_ON_BANANA_PEEL',
+});
+
+const watchTheNews = () => ({
+  type: 'WATCH_THE_NEWS',
+});
+
+const winTheLottery = () => ({
+  type: 'WIN_THE_LOTTERY',
+});
 
 
-export default class Person extends React.Component {
-  state = {
-    mentalHealth: 100,
-    physicalHealth: 100,
+// we will export a connected Person component
+// that is wrapped in higher order component called connect
+// at the bottom of the file
+export /* default */ class Person extends React.Component {
+  // state = {
+  //   mentalHealth: 100,
+  //   physicalHealth: 100,
+  // }
+/*
+  sliponBananaPeel = () => {
+    //  REDUX way of "yelling" that this action was dispatched
+    this.props.dispatch({ type: 'SLIP_ON_BANANA_PEEL' });
+
+    // this.setState(st => ({
+    //   physicalHealth: st.physicalHealth - 10,
+    // }));
   }
 
+  watchTheNews = () => {
+    this.props.dispatch({ type: 'WATCH_THE_NEWS' });
+
+    // this.setState(st => ({
+    //   mentalHealth: st.mentalHealth - 10,
+    // }));
+  }
+
+  winTheLottery = () => {
+    this.props.dispatch({ type: 'WIN_THE_LOTTERY' });
+
+    // this.setState(st => ({
+    //   mentalHealth: st.mentalHealth + 20,
+    //   physicalHealth: st.physicalHealth + 10,
+    // }));
+  }
+*/
   render() {
     return (
       <div>
-        <div>physical health: {this.state.physicalHealth}</div>
-        <div>mental health: {this.state.mentalHealth}</div>
+        <div>physical health: {this.props.physicalHealth}</div>
+        <div>mental health: {this.props.mentalHealth}</div>
 
-        <button>Slip on banana peel</button>
-        <button>Watch the news</button>
-        <button>Win the Lottery</button>
+        <button onClick={this.props.sliponBananaPeel}>Slip on banana peel</button>
+        <button onClick={this.props.watchTheNews}>Watch the news</button>
+        <button onClick={this.props.winTheLottery}>Win the Lottery</button>
       </div>
     );
   }
 }
+
+// mapDispatchToProps removes the necessity to call dispatch inside the Person
+// component and you can add it to your action callers with this function through PROPS
+// this makes the Person more reusable and purely visual component
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    sliponBananaPeel,
+    watchTheNews,
+    winTheLottery,
+  }, dispatch);
+}
+
+
+// in this function you can select which state you want to pass to
+// specific component
+function mapStateToProps(state) {
+  return {
+    physicalHealth: state.physicalHealth,
+    mentalHealth: state.mentalHealth,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Person);
